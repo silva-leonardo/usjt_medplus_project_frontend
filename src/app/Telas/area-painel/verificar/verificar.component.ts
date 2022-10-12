0e8017a951e80a7920f93fbaaa266db6aa9ac3c4
silva-leonardo <leonardo_silva7843@hotmail.com>
2022-10-12 14:04:21 -0300

    feat: Conclusão da tela de Cadastro e a inserção da responsividade mobile

---
+++ src/app/Telas/area-painel/verificar/verificar.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-verificar',
+  templateUrl: './verificar.component.html',
+  styleUrls: ['./verificar.component.css']
+})
+export class VerificarComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
---
+++ src/app/Telas/tela-cadastro/tela-cadastro.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-cadastro',
+  templateUrl: './tela-cadastro.component.html',
+  styleUrls: ['./tela-cadastro.component.css']
+})
+export class telaCadastroComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
